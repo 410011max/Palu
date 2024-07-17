e053e5f3570bd8eb0642e51820ded22656e55b3f
410011max <410011max@gmail.com>
2024-07-17 20:13:41 +0800

    update scripts

--- scripts.sh
+++ scripts.sh
@@ -4,8 +4,20 @@ CUDA_VISIBLE_DEVICES=0 python profile_attention.py \
     --prompt_len 65536 | tee -a logs/palu.log
 
 CUDA_VISIBLE_DEVICES=1 python profile_attention.py \
-    --rank_k 1024 --rank_v 1024 --group_size 4 \
+    --rank_k 1024 --rank_v 3072 --group_size 4 \
     --prompt_len 65536 --palu | tee -a logs/palu.log
 
+CUDA_VISIBLE_DEVICES=0 python profile_attention.py \
+    --rank_k 1024 --rank_v 3072 --group_size 4 \
+    --prompt_len 65536 --palu_no_rope | tee -a logs/palu_no_rope.log
 
+conda activate /home/scott306lr_l/envs/pytorch/
 conda activate /home/max410011_l/.conda/envs/palu/
+
+
+
+
+CUDA_VISIBLE_DEVICES=0 python profile_attention.py \
+    --rank_k 1024 --rank_v 3072 --group_size 4 \
+    --prompt_len 65536 --palu
+
