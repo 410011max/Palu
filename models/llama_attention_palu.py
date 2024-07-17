85ad6a257bf18cfe6631a2927cd071d6c7e4b0fa
410011max <410011max@gmail.com>
2024-07-17 20:14:19 +0800

    add palu without RoPE

--- models/llama_attention_palu.py
+++ models/llama_attention_palu.py
@@ -12,7 +12,7 @@ from transformers.models.llama.modeling_llama import (
     LlamaConfig, logger, LlamaRotaryEmbedding,
 )
 
-from kernel.abx_rope_share_head import abx as recompute_k_gemv
+from kernel.abx_rope import abx as recompute_k_gemv
 
 
 
@@ -275,3 +275,170 @@ class LlamaAttention_PALU(nn.Module):
 
         return attn_output, attn_weights, past_key_value
 
+class LlamaAttention_PALU_no_rope(nn.Module):
+    """Multi-headed attention from 'Attention Is All You Need' paper"""
+
+    def __init__(self, config: LlamaConfig, layer_idx: Optional[int] = None):
+        super().__init__()
+        self.config = config
+        self.layer_idx = layer_idx
+        if layer_idx is None:
+            logger.warning_once(
+                f"Instantiating {self.__class__.__name__} without passing `layer_idx` is not recommended and will "
+                "to errors during the forward call, if caching is used. Please make sure to provide a `layer_idx` "
+                "when creating this class."
+            )
+
+        self.attention_dropout = config.attention_dropout
+        self.hidden_size = config.hidden_size
+        self.num_heads = config.num_attention_heads
+        self.head_dim = self.hidden_size // self.num_heads
+        self.num_key_value_heads = config.num_key_value_heads
+        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+        self.max_position_embeddings = config.max_position_embeddings
+        self.rope_theta = config.rope_theta
+        self.is_causal = True
+
+        if (self.head_dim * self.num_heads) != self.hidden_size:
+            raise ValueError(
+                f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
+                f" and `num_heads`: {self.num_heads})."
+            )
+
+        # self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=config.attention_bias)
+        # self.k_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=config.attention_bias)
+        # self.v_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=config.attention_bias)
+        # self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias=config.attention_bias)
+        
+        # self.rank_k = [128, 128, 128, 128, 128, 128, 128, 128]
+        # self.rank_v = [128, 128, 128, 128, 128, 128, 128, 128]
+        # self.group_size = 4
+        # self.num_groups = 8
+        self.rank_k = config.rank_k
+        self.rank_v = config.rank_v
+        self.group_size = config.group_size
+        self.num_groups = config.num_groups
+        self.lr_head_dim = self.head_dim * self.rank_v // self.hidden_size
+        self.fused_group_dim = self.rank_v // self.num_groups
+        self.fused_hidden_dim = self.fused_group_dim * self.num_heads
+        print(f'fused_group_dim: {self.fused_group_dim}')
+        print(f'fused_hidden_dim: {self.fused_hidden_dim}')
+        self.q_proj = nn.Linear(self.hidden_size, self.fused_hidden_dim, bias=config.attention_bias)
+        self.k_proj = nn.Linear(self.hidden_size, self.rank_k, bias=config.attention_bias)
+        self.v_proj = nn.Linear(self.hidden_size, self.rank_v, bias=config.attention_bias)
+        self.o_proj = nn.Linear(self.fused_hidden_dim, self.hidden_size, bias=config.attention_bias)
+        
+        self._init_rope()
+
+    def _init_rope(self):
+        if self.config.rope_scaling is None:
+            self.rotary_emb = LlamaRotaryEmbedding(
+                self.head_dim,
+                max_position_embeddings=self.max_position_embeddings,
+                base=self.rope_theta,
+            )
+        else:
+            raise ValueError(f"Not support RoPE with scaling.")
+
+    def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
+        return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if "padding_mask" in kwargs:
+            warnings.warn(
+                "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        # key_states = self.k_proj(hidden_states)
+        # value_states = self.v_proj(hidden_states)
+        key_h_states = self.k_proj(hidden_states)
+        value_h_states = self.v_proj(hidden_states)
+
+        # query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        # key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        # value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.fused_group_dim).transpose(1, 2)
+        key_h_states = key_h_states.view(bsz, q_len, self.num_groups, self.fused_group_dim).transpose(1, 2)
+
+        
+        # num_heads, 1, rank
+        # num_groups, rank, 1
+
+
+        value_h_states = value_h_states.view(bsz, q_len, self.num_groups, self.fused_group_dim).transpose(1, 2)
+
+        # kv_seq_len = key_states.shape[-2]
+        kv_seq_len = key_h_states.shape[-2]
+        if past_key_value is not None:
+            if self.layer_idx is None:
+                raise ValueError(
+                    f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__} "
+                    "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
+                    "with a layer index."
+                )
+            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+        
+        # cos, sin = self.rotary_emb(query_states, seq_len=kv_seq_len)
+        # query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+        if past_key_value is not None:
+            # cache_kwargs = {"sin": sin, "cos": cos}  # Specific to RoPE models
+            # key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+            key_h_states, value_h_states = past_key_value.update(key_h_states, value_h_states, self.layer_idx)
+
+        # TODO: Check if it can support the grouped query attention or not
+        # Grouped-query attention
+        # key_states = repeat_kv(key_states, self.num_key_value_groups)
+        # value_states = repeat_kv(value_states, self.num_key_value_groups)
+        # key_h_states = repeat_kv(key_h_states, self.num_key_value_groups)
+        # value_h_states = repeat_kv(value_h_states, self.num_key_value_groups)
+
+
+        # attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+        attn_weights = torch.matmul(query_states, key_h_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+        attn_weights = attn_weights.view(bsz, self.num_heads, q_len, kv_seq_len)
+
+
+        if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+            raise ValueError(
+                f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+                f" {attn_weights.size()}"
+            )
+
+        if attention_mask is not None:
+            if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                raise ValueError(
+                    f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                )
+            attn_weights = attn_weights + attention_mask
+
+        # Upcast attention to fp32
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+        # attn_output = torch.matmul(attn_weights, value_states)
+
+        attn_weights = attn_weights.reshape(bsz, self.num_groups, q_len * self.group_size, kv_seq_len)
+        attn_output = torch.matmul(attn_weights, value_h_states)
+
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.reshape(bsz, q_len, -1)
+
+        attn_output = self.o_proj(attn_output)
+        
+        
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
